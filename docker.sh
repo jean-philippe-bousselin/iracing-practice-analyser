06c0c599590ada202d12c44090ae6f3dbe283cd6
jean-philippe-bousselin <jp.bousselin@gmail.com>
2016-09-24 16:28:21 +0200

    Added node/gulp/bower image

--- docker.sh
+++ docker.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+# project image
 docker build -t ipa .
 docker run -d -p 80:80 -p 3306:3306 -v $(pwd)/src:/app ipa
+
+# node/gulp/bower image
+docker pull evolution7/nodejs-bower-gulp
+# docker exec -it 001fe54b06e6 bash
+
+
+# docker run --rm -v `pwd`:/app evolution7/nodejs-bower-gulp npm install
+# docker run --rm -v `pwd`:/app evolution7/nodejs-bower-gulp bower install --allow-root
+# docker run --rm -v `pwd`:/app evolution7/nodejs-bower-gulp gulp build
