ced13ff4f171dbb81f0670a3773fb28b41aa0008
jean-philippe-bousselin <jp.bousselin@gmail.com>
2016-09-25 18:39:57 +0200

    Automated db schema loading on container startup

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 
 # project image
 docker build -t ipa .
-docker run -d -p 80:80 -p 3306:3306 -v $(pwd)/src:/app ipa
+docker run -d --name="ipa_app" -p 80:80 -p 3306:3306 -v $(pwd)/src:/app ipa
 
 # node/gulp/bower image
 docker pull evolution7/nodejs-bower-gulp
@@ -12,3 +12,7 @@ docker pull evolution7/nodejs-bower-gulp
 # docker run --rm -v `pwd`:/app evolution7/nodejs-bower-gulp npm install
 # docker run --rm -v `pwd`:/app evolution7/nodejs-bower-gulp bower install --allow-root
 # docker run --rm -v `pwd`:/app evolution7/nodejs-bower-gulp gulp build
+
+# load database
+docker exec -it $(docker ps -aqf "name=ipa_app") mysql -u root -e "CREATE DATABASE ipa"
+docker exec -it $(docker ps -aqf "name=ipa_app") mysql -u root -e "use ipa; source /app/db/ipa.sql;"
